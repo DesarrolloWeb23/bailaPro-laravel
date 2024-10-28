17089d972d4dd256a74fcd9d1fda1339682eec22
fabianrojasSab <Desarrollo1@pijaossalud.com.co>
2024-10-28 11:09:31 -0500

    fix
    Se corrige complemento x-card para pasar por parametro la ruta a la que se desea ir

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,3 +1,3 @@
 <div class="grid grid-cols-2 p-6 lg:p-8 bg-white border-b border-gray-200">
-    <x-card tittle="Registrar usuario" description="Realizar el registro de nuevos usuarios"/>
+    <x-card tittle="Registrar usuario" description="Realizar el registro de nuevos usuarios" ruta="usr.r"/>
 </div>
