8aa5687e8fd3935b0abd1f73bb57bc0d7b2bbd26
fabianrojasSab <Desarrollo1@pijaossalud.com.co>
2024-10-29 13:10:49 -0500

    feat
    se agregan validaciones de permisos en la dashboard para ingresar deacuerdo al estado y el rol

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,8 +1,16 @@
 <div class="grid grid-cols-2 p-6 lg:p-8 bg-white border-b border-gray-200">
-    <x-card tittle="Registrar usuario" description="Realizar el registro de nuevos usuarios" ruta="usr.r"/>
-    <x-card tittle="Registrar profesor" description="Realizar el registro, edicion y ver el listado de los profesores" ruta="tch.r"/>
-    <x-card tittle="Registrar estudiantes" description="Realizar el registro, edicion y ver el listado de los alumnos" ruta="std.r"/>
-    <x-card tittle="Registrar pagos" description="Realizar registro de pagos" ruta="pym.r"/>
-    <x-card tittle="Registrar clases" description="Realizar registro de las clases programadas por los profesores" ruta="lsn.r"/>
-    <x-card tittle="Registrar Inscripciones" description="Realizar la inscripcion a las clases registradas" ruta="ncp.r"/>
+    @if(auth()->user()->estado_id == '1')
+        @if(auth()->user()->rol_id == '3')
+            <!-- Contenido para administradores -->
+            <x-card tittle="Registrar usuario" description="Realizar el registro de nuevos usuarios" ruta="usr.r"/>
+            <x-card tittle="Registrar profesor" description="Realizar el registro, edicion y ver el listado de los profesores" ruta="tch.r"/>
+            <x-card tittle="Registrar estudiantes" description="Realizar el registro, edicion y ver el listado de los alumnos" ruta="std.r"/>
+            <x-card tittle="Registrar pagos" description="Realizar registro de pagos" ruta="pym.r"/>
+            <x-card tittle="Registrar clases" description="Realizar registro de las clases programadas por los profesores" ruta="lsn.r"/>
+            <x-card tittle="Registrar Inscripciones" description="Realizar la inscripcion a las clases registradas" ruta="ncp.r"/>    
+        @endif
+    @else
+    <div>Usted se encuentra en estado PRE-REGISTRO, por favor comuniquese con su administrador</div>
+    @endif
+
 </div>
